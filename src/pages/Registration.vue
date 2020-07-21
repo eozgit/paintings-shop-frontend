97babd5d6bee96c18ed19cf8bb6915f3f8b5534e
Enis Ozgit <eozgit@gmail.com>
2020-07-21 10:38:22 +0100

    Add registration page
    Remove unused items from layout

---
+++ src/pages/Registration.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+      @submit="onSubmit"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="username"
+        label="Username *"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Username cannot be empty']"
+      />
+
+      <q-input
+        filled
+        type="email"
+        v-model="email"
+        label="Email address *"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Email address cannot be empty']"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="password"
+        label="Password *"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Password cannot be empty']"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="confirm"
+        label="Confirm password *"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Password confirmation cannot be empty']"
+      />
+
+      <div>
+        <q-btn label="Register" type="submit" color="primary"/>
+      </div>
+
+      <ul v-if="hints.length" class="warnings text-negative">
+        <li v-for="(hint, i) in hints" :key="i">
+          {{ hint }}
+        </li>
+      </ul>
+    </q-form>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from '@vue/composition-api'
+
+interface FormData {
+      username: string,
+      email: string,
+      password: string,
+      confirm: string,
+      hints: string[]
+}
+
+interface RegistrationResponse {
+    key?: string
+    username?: string[],
+    email?: string[],
+    password1?: string[],
+    password2?: string[],
+}
+
+export default defineComponent({
+  name: 'Registration',
+  setup () {
+    const data: FormData = {
+      username: '',
+      email: '',
+      password: '',
+      confirm: '',
+      hints: []
+    }
+    const formData = reactive(data)
+
+    async function onSubmit () {
+      const headers = new Headers()
+      headers.append('Content-Type', 'application/json')
+
+      const body = JSON.stringify({
+        username: formData.username,
+        email: formData.email,
+        password1: formData.password,
+        password2: formData.confirm
+      })
+
+      const requestOptions = {
+        method: 'POST',
+        headers,
+        body
+      }
+
+      const response = await fetch('http://localhost:8000/rest-auth/registration/', requestOptions)
+
+      const json = await response.json() as RegistrationResponse
+
+      formData.hints = []
+      if (json.username) {
+        formData.hints = [...formData.hints, ...json.username]
+      }
+      if (json.email) {
+        formData.hints = [...formData.hints, ...json.email]
+      }
+      if (json.password1) {
+        formData.hints = [...formData.hints, ...json.password1]
+      }
+      if (json.password2) {
+        formData.hints = [...formData.hints, ...json.password2]
+      }
+    }
+    return { ...toRefs(formData), onSubmit }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.warnings {
+  list-style: none;
+  font-size: small;
+}
+</style>
